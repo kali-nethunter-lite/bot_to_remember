e6268968e6779183ae3d9ca7d6a590d2553d9880
Nethunter <162314228+kali-nethunter-lite@users.noreply.github.com>
2025-05-11 20:21:23 +0300

    Update system.py

--- app/database/system.py
+++ app/database/system.py
@@ -16,32 +16,31 @@ update = any(
     for key in default_config)
 
 if update:
-    print("Привет! Давай настроим бота...")
-    config["token"] = input("Введите токен бота: ").strip()
-    config["password"] = input("Введите новый пароль бота: ").strip()
+    print("Привіт! Давай налаштуємо бота...")
+    config["token"] = input("Введіть токен бота: ").strip()
+    config["password"] = input("Введіть новий пароль бота: ").strip()
     while True:
-        owners_input = input("Введите ID владельцев (12345, 45678, 67890): ").strip().split(",")
+        owners_input = input("Введіть ID власників (12345, 45678, 67890): ").strip().split(",")
         try:
             config["owners_id"] = [int(owner.strip()) for owner in owners_input]
             break
         except ValueError:
-            print("Ошибка: все ID должны быть целыми числами. Попробуйте снова.")
+            print("Помилка: всі ID мають бути цілими числами. Спробуйте ще раз.")
 
     while True:
-        admins_input = input("Введите ID администраторов без ID владельцев (12345, 45678, 67890): ").strip().split(",")
+        admins_input = input("Введіть ID адміністраторів без ID власників (12345, 45678, 67890): ").strip().split(",")
         try:
             config["admins_id"] = list(set([int(admin.strip()) for admin in admins_input if admin.strip()] + config["owners_id"]))
             break
         except ValueError:
-            print("Все ID должны быть целыми числами. Попробуйте снова.")
+            print("Помилка: всі ID мають бути цілими числами. Спробуйте ще раз.")
 
     with open(config_path, "w", encoding="utf-8") as f:
         json.dump(config, f, indent=4, ensure_ascii=False)
 
-    print("Данные обновлены.\n")
+    print("Дані оновлені.\n")
 
 token = config["token"]
 password = config["password"]
 admins_id = list(map(int, config["admins_id"]))
 owners_id = list(map(int, config["owners_id"]))
-
