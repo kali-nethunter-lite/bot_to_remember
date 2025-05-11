b7190e3a68b1f7b3c2c6c05eca014e752c3da629
Nethunter <162314228+kali-nethunter-lite@users.noreply.github.com>
2025-05-11 20:15:14 +0300

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -2,17 +2,17 @@ from aiogram.types import (InlineKeyboardMarkup, InlineKeyboardButton)
 
 menu = InlineKeyboardMarkup(inline_keyboard=[[
                 InlineKeyboardButton(text='По телефону', callback_data='search_number'),
-                InlineKeyboardButton(text='По ФИО', callback_data='search_fio')]])
+                InlineKeyboardButton(text='По ПІБ', callback_data='search_fio')]])
 
 admin_menu = InlineKeyboardMarkup(inline_keyboard=[[
                 InlineKeyboardButton(text='По телефону', callback_data='search_number'),
-                InlineKeyboardButton(text='По ФИО', callback_data='search_fio')],
-                [InlineKeyboardButton(text='Панель администратора', callback_data='admin_panel')]])
+                InlineKeyboardButton(text='По ПІБ', callback_data='search_fio')],
+                [InlineKeyboardButton(text='Панель адміністратора', callback_data='admin_panel')]])
 
 admin_panel = InlineKeyboardMarkup(inline_keyboard=[
-                [InlineKeyboardButton(text='Добавить пользователя', callback_data='add_user')],
-                [InlineKeyboardButton(text='Бан', callback_data='ban_user'),
-                InlineKeyboardButton(text='Разбан', callback_data='unban_user')],
+                [InlineKeyboardButton(text='Додати інформацію', callback_data='add_user')],
+                [InlineKeyboardButton(text='Блокування', callback_data='ban_user'),
+                InlineKeyboardButton(text='Розблокування', callback_data='unban_user')],
                 [InlineKeyboardButton(text='Назад', callback_data='back_to_main')]])
 
 back_to_admin_menu = InlineKeyboardMarkup(inline_keyboard=[
@@ -20,4 +20,3 @@ back_to_admin_menu = InlineKeyboardMarkup(inline_keyboard=[
 
 back_keyboard = InlineKeyboardMarkup(inline_keyboard=[
                 [InlineKeyboardButton(text="Назад", callback_data="back_to_main")]])
-
